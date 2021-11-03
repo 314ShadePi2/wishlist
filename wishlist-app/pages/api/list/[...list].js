36f443a12cec9df4c4bc925c39348d94175639a5
314ShadePi <craft-purple@aepschmitt.dk>
2021-11-03 17:40:25 +0100

    Update wishlist-app/pages/api/list/[...list].js to catch fetch errors

--- wishlist-app/pages/api/list/[...list].js
+++ wishlist-app/pages/api/list/[...list].js
@@ -8,9 +8,13 @@ export default async function handler(req, res) {
     if(pathToList.endsWith('.json')==false){
         res.status(400).json({ error: 'Error: Please put .json after filename', status: 400 , providedPath: list.join('/'), providedFullPath: pathToList, possibleTruePath: `${list.join('/')}.json`, possibleTrueFullPath: `${pathToList}.json` })
     } else {
-        const response = await fetch(urlToList);
-        const data = await response.json();
-        res.json(data)
+        try {
+            const response = await fetch(urlToList);
+            const data = await response.json();
+            res.status(200).json(data)
+        } catch (error) {
+            res.status(400).json({ error: error })
+        }
 
     }
 }
